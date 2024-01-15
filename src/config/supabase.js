0346a012859bb625d1918401ee1c4eb96af91b15
Kevin Knights <kevinknights29@hotmail.com>
2024-01-15 09:15:55 -0500

    feat: added supabase client init

---
+++ src/config/supabase.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const {createClient} = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+/**
+ * The Supabase client for interacting with the Supabase API.
+ * @type {SupabaseClient}
+ */
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
